5fbc27a260694fd5cc6ac0ef01571327a88da649
Armando Urquiola <patchamama.go@gmail.com>
2024-11-26 22:13:20 +0100

    refactoring with navbar done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,40 +16,7 @@ function App() {
       <Header />
 
       <div className='App'>
-        <nav className='navbar navbar-expand-lg navbar-light bg-light fixed-top'>
-          <div className='container'>
-            <Link className='navbar-brand' to={'/sign-in'}>
-              C0₂nscious
-            </Link>
-            {/* Toggler button for small screens */}
-            <button
-              className='navbar-toggler'
-              type='button'
-              data-bs-toggle='collapse'
-              data-bs-target='#navbarSupportedContent'
-              aria-controls='navbarSupportedContent'
-              aria-expanded='false'
-              aria-label='Toggle navigation'
-            >
-              <span className='navbar-toggler-icon'></span>
-            </button>
-            {/* Collapsible menu */}
-            <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-              <ul className='navbar-nav ms-auto'>
-                <li className='nav-item'>
-                  <Link className='nav-link' to={'/login'}>
-                    Login
-                  </Link>
-                </li>
-                <li className='nav-item'>
-                  <Link className='nav-link' to={'/signup'}>
-                    Sign up
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+       <Navbar />
 
         <div className='auth-wrapper'>
           <div className='auth-inner'>
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,10 +2,40 @@ import { Link } from 'react-router-dom'
 export const Navbar = () => {
   return (
     <>
-      <Link to='/'>Home</Link>
-      <Link to='/login'>Login</Link>
-      <Link to='/singup'>Sign Up</Link>
-      <Link to='/about'>About</Link>
+      <nav className='navbar navbar-expand-lg navbar-light bg-light fixed-top'>
+        <div className='container'>
+          <Link className='navbar-brand' to={'/sign-in'}>
+            C0₂nscious
+          </Link>
+          {/* Toggler button for small screens */}
+          <button
+            className='navbar-toggler'
+            type='button'
+            data-bs-toggle='collapse'
+            data-bs-target='#navbarSupportedContent'
+            aria-controls='navbarSupportedContent'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <span className='navbar-toggler-icon'></span>
+          </button>
+          {/* Collapsible menu */}
+          <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+            <ul className='navbar-nav ms-auto'>
+              <li className='nav-item'>
+                <Link className='nav-link' to={'/login'}>
+                  Login
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link className='nav-link' to={'/signup'}>
+                  Sign up
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   )
 }
