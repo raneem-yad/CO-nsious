e4db262ea3f28d6990869bb38e9a02f53f05a1c2
raneem <eng.raneem.yad@gmail.com>
2024-11-27 15:40:21 +0100

    fix homepage static links issue in the backend

--- backend/conscious_backend/urls.py
+++ backend/conscious_backend/urls.py
@@ -20,6 +20,8 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 from conscious_backend import views
 
 schema_view = get_schema_view(
@@ -57,4 +59,4 @@ urlpatterns = [
     path('measures/', include('co2measures.urls')), # Required for REST apis using ListCreateAPIView 
     path('action/', include('action.urls')),
     path('employeeprofile/', include('employeeprofile.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
